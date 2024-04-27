918391623d51faffde03f784dc242a980116de7f
eduardo Alves <eduardo.telecomrbs@gmail.com>
2024-04-27 20:24:25 -0300

    :sparkles:novo recurso commands_2

--- cypress/e2e/specE2E.cy.js
+++ cypress/e2e/specE2E.cy.js
@@ -17,12 +17,22 @@ describe('test E2E Realizando compra de produto com sucesso', function() {
     //Seleciona documento, valida se documento está sendo realmente selecionado
     cy.get('input[type="file"]').invoke('attr', 'style', 'display: block').selectFile('./cypress/fixtures/Mr_bean.png')
     .should(function($input) {
-      expect($input[0].files[0].name).to.equal('Mr_bean.png')
+    expect($input[0].files[0].name).to.equal('Mr_bean.png')
     })
     //Submetendo formulário de cadastro e validadno mensagem de cadastro com sucesso
     cy.get('button[type=submit]').click();
     cy.get('.swal2-html-container').should('be.visible').and('contain.text', 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.');
     cy.screenshot('cadastro_entregador_sucesso');
     });
+
+  it.only('não deve cadastrar entregador com cpf invalido', function(){
+    cy.filMandatoryFields2()
+    cy.get('input[type="file"]').invoke('attr', 'style', 'display: block').selectFile('./cypress/fixtures/Mr_bean.png')
+    .should(function($input) {
+    expect($input[0].files[0].name).to.equal('Mr_bean.png');
+      })
+      cy.screenshot('cadastro_entregador_insucesso');
   })
 
+  
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,3 +14,14 @@ Cypress.Commands.add('validationofmandatoryfields', function(){
     cy.get('input[name=district]').should('be.visible').and('have.value', 'Setor Residencial Leste (Planaltina)');
     cy.get('input[name=city-uf]').should('be.visible').and('have.value', 'Brasília/DF');
 })
+
+Cypress.Commands.add('filMandatoryFields2', function(){
+    cy.get('input[name=name]').type('edauardo');
+    cy.get('input[name=cpf]').type('000000141ff');
+    cy.get('input[name=email]').type('EDU@GMAIL.COM');
+    cy.get('input[name=whatsapp]').type('6198523654');
+    cy.get('input[name=postalcode]').type('73357005');
+    cy.get('input[value="Buscar CEP"]').click();
+    cy.get('input[name=address-number]').type('15');
+    cy.get('input[name=address-details]').type('apt 01');
+})
